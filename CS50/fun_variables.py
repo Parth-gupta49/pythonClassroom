8154a4830b3ac58072e59dc84552c7a8a987aefe
Parth Gupta <nbparthgupta4959@gmail.com>
2024-01-13 10:33:16 +0000

    CS50_functions-variables_updated

--- CS50/fun_variables.py
+++ CS50/fun_variables.py
@@ -1,5 +1,15 @@
 # name = input("What is your name, babe\n")
+"""
+this is a multi line comment
 
+"""
+
+
+
+# what's inside print function ?
+
+# print(*obj,sep=' ',end='\n',file=sys.stdout, flush = False) # the actual print statement with the parameters in official docs
+# u can past any no of strings, seperator in b/w the arguments is a gap, and print statement is bydefault end via a newLine
 
 # print("hello, " + name) 
 # print("Myself",name,sep="???")
@@ -29,10 +39,74 @@
 
 
 
-name = input("Type : ").strip().title()
+# name = input("Type : ").strip().title()
 
 # split user name into first and last name
 
-first,last = name.split(" ")
-print(f"hello, {first}") 
+# firstName,lastName = name.split(" ")
+# print(f"Hello, {firstName}")
 
+
+# integers 
+
+# x = int(input("x = "))
+# y = int(input("y = "))
+# print(x+y)
+
+#floats
+
+# x = float(input("x = "))
+# y = float(input("y = "))
+
+# z = round(x+y,3) # round(number[, ndigits])
+# z = round(x+y)
+
+# print(f"{z:,}")  # put a comma after every 3 digits - US based
+
+# z = round(x/y,2)
+
+# z = x/y
+# print(f"{z:.2f}") # f string approach to solve the very same problem as we did before 
+
+
+
+
+# functions
+# def hello(to):       #define
+#     print("hello,",to)
+
+
+
+# if i want to give a default value to my "to " variable
+# def hello(to="world"):       #define
+#     print("hello,",to)
+
+# hello()
+# name = input("What's ur name? ")
+# hello(name)
+# print(name)
+
+
+# there seems to be a problem like suppose if you are having ur function down below where ur calling it you will eventually get an error cause it is not defined at that moment whre ur calling it so basically python does not even know that the function even exists
+# although there can be one solution to this one : 
+
+# def main():
+#     hello()
+#     name = input("What's ur name? ")
+#     hello(name)
+
+# def hello(to="world"):       #define
+#     print("hello,",to)
+    
+# main()
+
+
+# def main():
+#     x = int(input("x = "))
+#     print("x squared is ",square(x))
+    
+# def square(n):
+#     # return n**2
+#     return pow(n,2)
+
+# main()
