104062d1730f76e62a7bc0d9e4f3a129175163e2
Parth Gupta <nbparthgupta4959@gmail.com>
2024-01-18 05:21:13 +0000

    lab2- regex program

---
+++ prac/regex.py
@@ -0,0 +1,31 @@
+
+
+import re
+text = "Steve Smith is a Rockstar. Yup!! he is a Rockstar"
+pattern = r"[A-Z]+ockstar"
+
+# match function 
+match = re.search(pattern,text)
+if match:
+    print("We got it")   
+else:
+    print("Its not there mate")
+
+
+# finditer function will take care of every pattern it finds and gives the span for every find. It can be used as alternative of search function as it only finds the find match 
+matches = re.finditer(pattern,text)
+for match in matches:
+    print(match)
+
+
+# findall function 
+match = re.findall("is",text)
+print(match)
+
+# split function
+match = re.split("\s",text,3)
+print(match)
+
+# sub function 
+match = re.sub("\s","-space-",text,2)
+print(match)
