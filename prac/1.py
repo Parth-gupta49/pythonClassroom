f405e5dc41c0ee13317909fd68c2de6c89ca696a
Parth Gupta <nbparthgupta4959@gmail.com>
2024-01-19 05:21:37 +0000

    lab1 oops program created

--- prac/1.py
+++ prac/1.py
@@ -1,16 +1,43 @@
 
 class Human:
     population=0
-    def __init__(self,name,age,married):
-        self.name = name
+    def __init__(self,gender,age):
+        self.gender = gender
         self.age = age
-        self.married = married
         Human.population+=1
-    def printDetails(self):
-        print(f"{self.name} who ages {self.age} \n Married status : {self.married}")
+    @staticmethod
+    def speak():
+        print("Human is speaking")
+    
+    def studySubject(self,subject):
+        print(f"Human studies {subject}")
 
-human1 = Human("Parth",18,False)
-human2 = Human("Steve",35,True)
-human1.printDetails()
-human2.printDetails()
-print(Human.population)
+class Human1(Human):
+    def __init__(self,name, gender, age):
+        super().__init__(gender, age)
+        self.name = name
+
+    def setPersonalDetails(self,married,subject,athlete):
+        self.married = married
+        self.subject = subject
+        self.athlete = athlete
+    def getSubject(self):
+        print(f"{self.name} studies {self.subject}")
+    def getMarriedStatus(self):
+        return self.married
+    
+
+    def getAllDetails(self):
+        print(f"Name : {self.name}\nGender : {self.gender}\nAge : {self.age}\n",end="")
+        if(self.getMarriedStatus()):
+            print("Married Status : Married")
+        else:
+            print("Married Status : Unmarried")
+
+obj1 = Human1("Steve","Male",19)
+obj1.setPersonalDetails(False,"JavaScript",True)
+obj1.getSubject()
+obj1.getAllDetails()
+
+
+
